12a71d78e9e4ce0d7ea7932724ad5b9cac088fb0
sanchxzzz <samuelsanchezluna11@gmail.com>
2025-11-29 11:13:50 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,125 +1,89 @@
-﻿// Lista masiva de personajes
-const personajes = [
-  // Futbolistas
-  "Lionel Messi", "Cristiano Ronaldo", "Neymar", "Mbappé", "James Rodríguez",
-  "Dembélé", "Karim Benzema", "Sergio Ramos", "Luis Suárez", "Zlatan Ibrahimović",
-  "Kevin De Bruyne", "Luka Modric", "Paul Pogba", "Thiago Silva", "Kylian Mbappé",
-  "Erling Haaland", "Robert Lewandowski", "Marcelo", "Alexis Sánchez", "Vinicius Jr.",
-  "Hakimi", "Casemiro", "Rodri", "Di María", "Ousmane Dembélé",
-
-  // Cantantes Trap/Reggaeton
-  "Bad Bunny", "J Balvin", "Kris R.", "Pirlo", "Blessd", "Ryan Castro", "GeezyDee",
-  "Feid", "Karol G", "Maluma", "Rauw Alejandro", "Anuel AA", "Trueno", "Ysy A",
-  "Duki", "Cazzu", "Neo Pistea", "Bhavi", "Khea", "Reykon", "Farina", "Nio Garcia",
-
-  // Actores
-  "Leonardo DiCaprio", "Will Smith", "Robert Downey Jr.", "Chris Hemsworth", 
-  "Zendaya", "Emma Watson", "Dwayne Johnson", "Scarlett Johansson", "Tom Holland",
-  "Gal Gadot", "Chris Evans", "Ryan Reynolds", "Natalie Portman", "Margot Robbie",
-
-  // Streamers
-  "Westcol", "Ninja", "Tfue", "Shroud", "Rubius", "Auronplay", "Ibai", 
-  "xQc", "Pokimane", "Myth", "Sodapoppin", "Valkyrae", "Luzu", "Coscu",
-
-  // Personajes de ficción
-  "Harry Potter", "Hermione Granger", "Ron Weasley", "Frodo", "Gandalf",
-  "Iron Man", "Spider-Man", "Batman", "Superman", "Wonder Woman",
-  "Thor", "Loki", "Hulk", "Black Panther", "Captain America"
+﻿const personajes = [
+    "Harry Potter", "Hermione Granger", "Ron Weasley", "Hagrid", "Dumbledore", "Voldemort",
+    "Batman", "Superman", "Flash", "Wonder Woman", "Aquaman", "Cyborg", "Joker", "Harley Quinn",
+    "Spider-Man", "Iron Man", "Captain America", "Hulk", "Thor", "Black Widow", "Hawkeye",
+    "Doctor Strange", "Black Panther", "Star-Lord", "Gamora", "Drax", "Rocket", "Groot",
+    "Loki", "Wanda Maximoff", "Vision", "Thanos",
+    "Goku", "Vegeta", "Gohan", "Trunks", "Piccolo", "Majin Buu", "Freezer",
+    "Naruto", "Sasuke", "Sakura", "Kakashi", "Itachi", "Jiraiya", "Madara", "Obito",
+    "Luffy", "Zoro", "Sanji", "Nami", "Usopp", "Chopper", "Robin", "Franky", "Brook", "Jinbe",
+    "Tanjiro", "Nezuko", "Zenitsu", "Inosuke", "Rengoku",
+    "Aang", "Zuko", "Katara", "Toph", "Sokka",
+    "Homer Simpson", "Bart Simpson", "Lisa Simpson", "Marge Simpson",
+    "Peter Griffin", "Stewie Griffin", "Brian Griffin",
+    "Scooby Doo", "Shaggy", "Fred", "Velma", "Daphne",
+    "Mario", "Luigi", "Yoshi", "Peach", "Bowser", "Toad", "Wario", "Waluigi",
+    "Link", "Zelda", "Ganondorf",
+    "Sonic", "Tails", "Knuckles", "Shadow",
+    "Freddy Fazbear", "Bonnie", "Chica", "Foxy", "Springtrap",
+    "Steve (Minecraft)", "Alex (Minecraft)", "Creeper", "Enderman", "Zombie",
+    "Kratos", "Atreus",
+    "Geralt de Rivia", "Yennefer", "Ciri",
+    "Master Chief", "Cortana",
+    "Lara Croft", 
+    "Pac-Man", 
+    "Ash Ketchum", "Pikachu", "Charizard", "Bulbasaur", "Squirtle", "Mewtwo",
+    "Messi", "Cristiano Ronaldo", "Neymar", "Mbappé", "Haaland", "Lewandowski", "Vinicius Jr",
+    "Pedri", "Gavi", "Lamine Yamal", "João Félix", "Ter Stegen", "Lewandowski",
+    "Kimmich", "Musiala", "Kane", "Modric", "Bellingham", "Salah", "Suárez",
+    "Canelo Álvarez", "Conor McGregor",
+    "Bad Bunny", "Karol G", "Feid", "J Balvin", "Maluma", "Shakira", "Rauw Alejandro",
+    "Duki", "Peso Pluma", "Natanael Cano", "Anuel", "Arcángel",
+    "Ariana Grande", "Taylor Swift", "Billie Eilish", "The Weeknd", "Drake", "Doja Cat",
+    "Travis Scott", "Kanye West",
+    "Santa Claus", "Duende", "Reno", "Grinch",
+    "Bob Esponja", "Patricio", "Calamardo", "Don Cangrejo", "Arenita", "Plankton",
+    "Shrek", "Burro", "Fiona", "Gato con Botas",
+    "Jack Sparrow",
+    "John Wick",
+    "James Bond",
+    "Rambo",
+    "Terminator",
+    "Rocky Balboa",
+    "Michael Jordan", "LeBron James", "Stephen Curry",
+    "Elon Musk", "Jeff Bezos", "Mark Zuckerberg",
+    "MrBeast", "AuronPlay", "Ibai", "TheGrefg", "ElRubius",
+    "Optimus Prime", "Bumblebee",
+    "Chucky", "Annabelle", "Pennywise",
+    "Wednesday Addams", "Morticia Addams",
+    "Barbie", "Ken",
+    "Bluey", "Bingo",
+    "Peppa Pig", "George Pig",
+    "Hércules", "Zeus", "Poseidón", "Hades",
+    "Thor (nórdico)", "Odín", "Loki (nórdico)",
+    "Cleopatra", "Napoleón", "Albert Einstein",
+    "Sherlock Holmes", "Dr. Watson",
+    "Willy Wonka", 
+    "Waldo",
+    "Chespirito", "El Chavo", "Quico", "La Chilindrina", "Don Ramón", "Doña Florinda",
+    "Goku Ultra Instinct", "Naruto Modo Sabio", "Luffy Gear 5",
+    "Miles Morales", "Peter Parker", "Spider-Gwen",
+    "Hulkbuster",
+    "Ant-Man", "Wasp",
+    "Shang-Chi",
+    "Moon Knight",
+    "Daredevil",
+    "Punisher",
+    "Ghost Rider",
+    "Mandalorian", "Grogu (Baby Yoda)",
+    "Darth Vader", "Luke Skywalker", "Yoda", "R2-D2", "C-3PO", "Han Solo", "Leia",
+    "Rick", "Morty",
+    "Finn", "Jake (Adventure Time)",
+    "Ben 10", "Kevin", "Gwen",
+    "Gumball", "Darwin",
+    "Ratatouille", "Remy", "Alfredo",
+    "Sully", "Mike Wazowski",
+    "Dash", "Violet", "Mr. Increíble", "Elastigirl",
+    "Wall-E", "EVA",
+    "Hipo", "Chimuelo",
+    "Marty", "Alex", "Gloria", "Melman",
+    "BMO", "Ice King",
+    "Ken Block",
+    "Spider-Man 2099",
+    "Hancock",
+    "Hércules (Disney)",
+    "Buzz Lightyear", "Woody", "Jessie",
+    "Mickey", "Minnie", "Donald", "Goofy",
+    "Pumba", "Timon", "Simba", "Mufasa", "Scar",
+    "Olaf", "Elsa", "Anna", "Kristoff"
 ];
-
-// Variables principales
-let jugadores = [];
-let impostores = [];
-let cantidadJugadores = 4; // por defecto
-let cantidadImpostores = 1; // por defecto
-let palabraInocente = "";
-
-// Iniciar juego
-function iniciarJuego() {
-    jugadores = [];
-    impostores = [];
-    
-    // Obtener nombre de jugadores
-    for (let i = 1; i <= cantidadJugadores; i++) {
-        let nombre = prompt(`Ingresa el nombre del jugador ${i}:`);
-        if (!nombre) nombre = `Jugador ${i}`;
-        jugadores.push({ nombre: nombre, palabra: "" });
-    }
-    
-    // Elegir impostores al azar
-    while (impostores.length < cantidadImpostores) {
-        let idx = Math.floor(Math.random() * jugadores.length);
-        if (!impostores.includes(idx)) impostores.push(idx);
-    }
-    
-    // Elegir palabra de los inocentes
-    palabraInocente = personajes[Math.floor(Math.random() * personajes.length)];
-    
-    // Asignar palabras
-    jugadores.forEach((jugador, idx) => {
-        if (impostores.includes(idx)) {
-            jugador.palabra = "Impostor";
-        } else {
-            jugador.palabra = palabraInocente;
-        }
-    });
-    
-    mostrarTurnos();
-}
-
-// Mostrar turnos de cada jugador
-function mostrarTurnos() {
-    jugadores.forEach(jugador => {
-        alert(`${jugador.nombre}, tu palabra es: ${jugador.palabra}`);
-    });
-    
-    alert("Ronda terminada, ahora a votar!");
-    votaciones();
-}
-
-// Votaciones
-function votaciones() {
-    let votos = [];
-    
-    jugadores.forEach((jugador, idx) => {
-        let nombres = jugadores.map((j, i) => `${i+1}: ${j.nombre}`).join("\n");
-        let voto = parseInt(prompt(`${jugador.nombre}, vota por quién eliminar:\n${nombres}`));
-        votos.push(voto-1);
-    });
-    
-    // Contar votos
-    let conteo = {};
-    votos.forEach(v => {
-        if (conteo[v] === undefined) conteo[v] = 0;
-        conteo[v]++;
-    });
-    
-    // Encontrar jugador con más votos
-    let maxVotos = -1;
-    let eliminado = -1;
-    for (let key in conteo) {
-        if (conteo[key] > maxVotos) {
-            maxVotos = conteo[key];
-            eliminado = parseInt(key);
-        }
-    }
-    
-    // Mostrar resultado
-    if (impostores.includes(eliminado)) {
-        alert(`¡Has eliminado al impostor! Era ${jugadores[eliminado].nombre}`);
-    } else {
-        alert(`¡Has eliminado a un inocente! Era ${jugadores[eliminado].nombre}`);
-    }
-    
-    alert("Fin de la partida");
-}
-
-// Cambiar cantidad de jugadores
-function setCantidadJugadores(cantidad) {
-    cantidadJugadores = Math.min(Math.max(cantidad,3),30); // mínimo 3, máximo 30
-}
-
-// Cambiar cantidad de impostores
-function setCantidadImpostores(cantidad) {
-    cantidadImpostores = Math.min(Math.max(cantidad,1),5); // mínimo 1, máximo 5
-}
